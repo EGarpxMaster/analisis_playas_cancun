937ff018e188a2986a06758871fe3ccc2a6e12a6
Emmanuel Garcia <200300595@ucaribe.edu.mx>
2025-09-01 11:06:38 -0500

    v.0.0.1b

--- pages/2_Tabla_datos.py
+++ pages/2_Tabla_datos.py
@@ -8,6 +8,6 @@ import pandas as pd
 st.subheader("Tabla de datos")
 st.write("Los datos que exploraremos están disponibles en la siguiente tabla:")
 
-df = pd.read_csv("../Datos/ocupacion_playas_cancun_clean.csv")
+df = pd.read_csv("datos/ocupacion_playas_cancun_clean.csv")
 st.dataframe(df)
 
